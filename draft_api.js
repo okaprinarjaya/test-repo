e39bb030d47314dacc5b87f842a7b61dc63ba5a2
Oka Prinarjaya <okaprinarjaya@gmail.com>
2018-05-28 01:03:25 +0700

    Changes a lot

--- draft_api.js
+++ draft_api.js
@@ -17,27 +17,11 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME
 });
 
-// Query 1
-const qry1 = (err, results) => {
-  return new Promise((resolve, reject) => {
-    if (err) {
-      reject(null);
-    } else {
-      resolve(results);
-    }
-  });
-};
-
-const qry2 = (err, results) => {
-  //
-};
-
-const qry3 = (err, results) => {
-  //
-};
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.post('/suara-masuk-draft', (req, resp) => {
-  pool.getConnection(async (err, conn) => {
+  pool.getConnection((err, conn) => {
     const rows = req.body.data;
 
     for (let x = 0; x < rows.length; x++) {
@@ -61,21 +45,24 @@ app.post('/suara-masuk-draft', (req, resp) => {
         "WHERE relawan_id = ? " +
         "AND kelurahan_id = ?";
 
-      const qry1Result = await conn.query(strQry1, [relawan_id], qry1);
-      const qry2Result = await conn.query(strQry2, [relawan_id, kelurahan_id], qry2);
-      // const qry3Result = await conn.query('', qry3);
+      conn.query(strQry1, [relawan_id], (err1, resultsQry1) => {
+        console.log('resultsQry1', resultsQry1);
 
-      console.log('qry1Result', qry1Result);
-      console.log('qry2Result', qry2Result);
-      resp.json({ result1: qry1Result, result2: qry2Result });
+        conn.query(strQry2, [relawan_id, kelurahan_id], (err2, resultsQry2) => {
+          console.log('resultsQry2', resultsQry2);
+          console.log('resultsQry1 bisa dipanggil disini', resultsQry1);
+
+          // Query 3 here
+          // --
+        });
+      });
+
+      resp.json({ hola: 'Holaaa hola hola holaaaaa' });
     }
 
     conn.release();
   });
 });
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 app.listen(port, () => {
   console.log('Server listening on port', port);
